623dd4ae6745a7cff00b37823b9b7830f113f5a2
Ribesg <Ribesg@yahoo.fr>
2014-06-29 11:44:04 +0200

    Initial Commit

---
+++ desktop/src/fr/ribesg/n/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.ribesg.n.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.ribesg.n.N;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new N(), config);
+	}
+}
